665e4716c7ce33955c87e7b7aca7b60e10c6ed44
Marko Kroflic <marko@mkr.si>
2014-07-24 19:00:39 +0200

    - return latest 50 records from dht22 logs collection. - HighCharts reference changes. - added dynamic chart data via socket io

--- plugins/dht22/index.js
+++ plugins/dht22/index.js
@@ -2,8 +2,12 @@ if (typeof define !== 'function') {
   var define = require('amdefine')(module);
 }
 
-define([ 'node-dht-sensor' ], function(dhtsensor) {
+function rand( from, to ) {
+    return Math.floor(Math.random()*(to-from+1)+from);
+}
 
+//define([ 'node-dht-sensor' ], function(dhtsensor) {
+define([], function() {
   /**
    * DHT22 Plugin. This plugin is able to control an DHT22 that is attached to the GPIO of the Raspberry PI
    *
@@ -39,7 +43,7 @@ define([ 'node-dht-sensor' ], function(dhtsensor) {
     });
     
     app.get('sockets').on('connection', function(socket) {
-	    console.log('socket connected');
+      socket.emit('dht22-logs', { hello: 'world' });
     });
     
   };
@@ -65,47 +69,47 @@ define([ 'node-dht-sensor' ], function(dhtsensor) {
         if ((!err) && (result.length > 0)) {
           result.forEach(function(item) {
             that.sensors[item._id] = item;
-	    
-  	    var sensor = { _id: item._id, timer: null,
-		    initialize: function() {
-		    		return dhtsensor.initialize(22, parseInt(item.pin));
-	    	    },
-		    read: function() {
-    			var readout = dhtsensor.read();
-			item = that.sensors[this._id + ''];
-              		if (isNaN(item.value)) {
-                		item.value = 0;
-              		}
-              		item.value = [ parseFloat((readout.temperature).toFixed(2)), parseFloat((readout.humidity).toFixed(2)) ];
-			if ( item.value[0] !== 0 && item.value[1] >= 1 && item.value[1] <= 100) { 
-	                        console.log('temperature: '+item.value[0]+'C, humidity: '+item.value[1]+'%');
-				that.values[item._id] = item.value;
-              			that.app.get('sockets').emit('dht22-sensor', {
-                			id: item._id,
-                			value: item.value
-              			});
-				that.app.get('db').collection(that.logs_collection, function(err, collection) {
-					collection.save({ sensor_id: item._id, timestamp: Math.round(new Date().getTime()/1000.0), temp: item.value[0], hum: item.value[1]}, function(err, object) {
-						if (err){
-          						console.warn(err.message);  // returns error if no matching object found
-      						}else{
-          						//console.dir(object);
-      						}	
-				  	});
-				});
-			}
-    			timer = setTimeout(function() {
-      				sensor.read();
-    			}, 60000);
-  		    },
-		    removeAllListeners: function() {
-			    clearTimeout(timer);
-		    }
-	    };
-	    sensor.initialize();
-  	    sensor.read();
-	    
-	    sensor._id = item._id;
+      
+        var sensor = { _id: item._id, timer: null,
+        initialize: function() {
+            return //dhtsensor.initialize(22, parseInt(item.pin));
+            },
+        read: function() {
+          var readout = { temperature: rand(20,30), humidity: rand(50,90) }; //dhtsensor.read();
+      item = that.sensors[this._id + ''];
+                  if (isNaN(item.value)) {
+                    item.value = 0;
+                  }
+                  item.value = [ parseFloat((readout.temperature).toFixed(2)), parseFloat((readout.humidity).toFixed(2)) ];
+      if ( item.value[0] !== 0 && item.value[1] >= 1 && item.value[1] <= 100) { 
+                          console.log('temperature: '+item.value[0]+'C, humidity: '+item.value[1]+'%');
+        that.values[item._id] = item.value;
+                    that.app.get('sockets').emit('dht22-sensor', {
+                      id: item._id,
+                      value: item.value
+                    });
+        that.app.get('db').collection(that.logs_collection, function(err, collection) {
+          collection.save({ sensor_id: item._id, timestamp: Math.round(new Date().getTime()/1000.0), temp: item.value[0], hum: item.value[1]}, function(err, object) {
+            if (err){
+                      console.warn(err.message);  // returns error if no matching object found
+                  }else{
+                      //console.dir(object);
+                  } 
+            });
+        });
+      }
+          timer = setTimeout(function() {
+              sensor.read();
+          }, 30000);
+          },
+        removeAllListeners: function() {
+          clearTimeout(timer);
+        }
+      };
+      sensor.initialize();
+        sensor.read();
+      
+      sensor._id = item._id;
             that.sensorList.push(sensor);
           });
         }
@@ -129,13 +133,23 @@ define([ 'node-dht-sensor' ], function(dhtsensor) {
     return callback(null, items);
   };
 
+  /**
+   * Manipulate the items array before sending chart data
+   *
+   * @method chartData
+   * @param {Array} items An array containing the items to be rendered
+   * @param {Function} callback The callback method to execute after manipulation
+   * @param {String} callback.err null if no error occured, otherwise the error
+   * @param {Object} callback.result The manipulated items
+   */
   DHT22.prototype.chartData = function(items, callback) {
     var that = this;
     var humArr = [], tempArr = []; 
     items.forEach(function(item)  {
-	tempArr.push([item.timestamp, item.temp]);	
- 	humArr.push([item.timestamp, item.hum]);
+      tempArr.push([item.timestamp, item.temp]);  
+      humArr.push([item.timestamp, item.hum]);
     });  
+    items = null;
     items = [tempArr, humArr];
     return callback(null, items);
   };
@@ -157,34 +171,36 @@ define([ 'node-dht-sensor' ], function(dhtsensor) {
       var method = req.body.method;
       if(method === "sensor") {
         this.app.get('db').collection("DHT22", function(err, collection) {
-            collection.find({}).toArray(function(err, items) {
-              if (!err) {
+          collection.find({}).toArray(function(err, items) {
+            if (!err) {
               that.beforeRender(items, function() {
                 res.send(200, items);
-                });
-              } else {
-              res.send(500, '[]');
-              }
               });
-            });
+            } else {
+              res.send(500, '[]');
+            }
+          });
+        });
       } else if(method === "logs") {
-	var sensor_id = req.body.sensor_id ? new require('mongodb').ObjectID(req.body.sensor_id) : null;      
-    	var timespan = req.body.timespan ? req.body.timespan : 24 * 3600;
-	if ( sensor_id == null ) {
-		res.send(500, '[]');
-	}
-	this.app.get('db').collection("DHT22_logs", function(err, collection) {
-	    console.log(" sensor id: " + sensor_id + " timespan " + timespan);
-	    collection.find({ sensor_id : sensor_id , timestamp : { $gt : (Math.round(new Date().getTime()/1000.0) - timespan)  } }, { _id : 0, sensor_id : 0 }).toArray(function(err, items) {
-              if (!err) {
+        var sensor_id = req.body.sensor_id ? new require('mongodb').ObjectID(req.body.sensor_id) : null;      
+        var timespan = req.body.timespan ? req.body.timespan : 24 * 3600;
+        if ( sensor_id == null ) {
+          res.send(500, '[]');
+        }
+        this.app.get('db').collection("DHT22_logs", function(err, collection) {
+          console.log(" sensor id: " + sensor_id + " timespan " + timespan);
+          collection.find({ sensor_id : sensor_id , 
+                              timestamp : { $gt : (Math.round(new Date().getTime()/1000.0) - timespan)  } }, 
+                          { _id : 0, sensor_id : 0 } ).sort({ timestamp : -1 }).limit( 50 ).toArray( function(err, items) {
+            if (!err) {
               that.chartData(items, function() {
                 res.send(200, items);
-                });
-              } else {
-              res.send(500, '[]');
-              }
               });
-            });
+            } else {
+              res.send(500, '[]');
+            }
+          });
+        });
       } else {
         next();
       }
--- plugins/dht22/public/js/dht22.js
+++ plugins/dht22/public/js/dht22.js
@@ -4,26 +4,26 @@ require([ "jquery", "/socket.io/socket.io.js" ], function() {
   
 
   var socket = io.connect();
-  var chart;
   /**
    * DHT22 sensor data received
    */
   socket.on('dht22-sensor', function(data) {
-  	console.log("dht22-sensor " + data);
+  	//console.log("dht22-sensor " + JSON.stringify(data));
     $('.value[data-id="' + data.id + 't"]').text(data.value[0]);
     $('.value[data-id="' + data.id + 'h"]').text(data.value[1]);
 
-    /*
+    var chart = $('#' + data.id + '-graph').highcharts();
+    
     d = new Date();
-    temp.push( [ d, data.value[0] ] );
-    hum.push( [ d, data.value[1] ] );
+    //temp.push( [ d, data.value[0] ] );
+    //hum.push( [ d, data.value[1] ] );
     var series = chart.series[0],
         shift = series.data.length > 20; // shift if the series is 
                                                  // longer than 20
     // add the point
     chart.series[0].addPoint([ d, data.value[0] ], true, shift);
-    //chart.series[1].addPoint([ d, data.value[1] ], true, shift);
-    */
+    chart.series[1].addPoint([ d, data.value[1] ], true, shift);
+    
   });
  
 
@@ -44,7 +44,7 @@ require([ "jquery", "/socket.io/socket.io.js" ], function() {
           },
           yAxis: {
             title: {
-                text: 'Value'
+                text: 'Temperature & Humidity'
             },
             plotLines: [{
                 value: 0,
@@ -65,7 +65,7 @@ require([ "jquery", "/socket.io/socket.io.js" ], function() {
         exporting: {
             enabled: false
         },
-          series: [{}, {}]
+          series: [{name: "Temperature"}, {name: "Humidity"}]
       };
 
       $.post( "/api/dht22", { "method": "logs", "sensor_id" : sensor_id }, function( records ) {
@@ -74,65 +74,12 @@ require([ "jquery", "/socket.io/socket.io.js" ], function() {
             options.series[0].data.push([  new Date(record.timestamp * 1000).getTime(), record.temp ]);
             options.series[1].data.push([  new Date(record.timestamp * 1000).getTime(), record.hum ]);
           });         
-          var chart = new Highcharts.Chart(options);
+          options.series[0].data.reverse();
+          options.series[1].data.reverse();
+          $('#' + sensor_id + '-graph').highcharts(options);
+          //var chart = new Highcharts.Chart(options);
       });
     });
   });
-/*
-  function requestData() {
-                // generate an array of random data
-                $.post( "/api/dht22", { "method": "logs", "sensor_id" : '53bc36f9e295089240c6c8b2' }, function( records ) {
-                    var t = [];
-                    records.forEach(function(record){
-                      t.push([ record.timestamp, record.temp ]);
-                    });
-                    chart.series[0].addPoint([ d, data.value[0] ], true, shift);
-                }, "json");                            
-  }
 
-    $('#53bc36f9e295089240c6c8b2-graph').highcharts({
-        chart: {
-            type: 'spline',
-            animation: Highcharts.svg, // don't animate in old IE
-            marginRight: 10,
-            events: {
-                load: requestData
-            }
-        },
-        title: {
-            text: 'DHT22'
-        },
-        xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 150
-        },
-        yAxis: {
-            title: {
-                text: 'Value'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            formatter: function() {
-                    return '<b>'+ this.series.name +'</b><br/>'+
-                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
-                    Highcharts.numberFormat(this.y, 2);
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        exporting: {
-            enabled: false
-        },
-        series: [{
-            name: 'Temperature',
-            data: []
-        }]
-    });
-*/
 });
